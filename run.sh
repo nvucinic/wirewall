acd128cb1362880192c99cf50a5db353fca83c34
Neven Vucinic <neven.vucinic@nvteh.com>
2023-10-21 22:58:13 +0200

    initial release

---
+++ run.sh
@@ -0,0 +1,29 @@
+!/bin/bash
+
+# Prereqs and docker
+sudo apt-get update &&
+    sudo apt-get install -yqq \
+         curl \
+         git \
+         apt-transport-https \
+         ca-certificates \
+         gnupg-agent
+
+# Install Docker repository and keys
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+        $(lsb_release -cs) \
+        stable" &&
+    sudo apt-get update &&
+    sudo apt-get install -yqq \
+         docker-ce \
+         docker-ce-cli \
+         containerd.io \
+         docker-compose-plugin
+
+# wirehole
+git clone https://github.com/nvucinic/wirewall.git &&
+    cd wirehole &&
+    docker compose up -d
